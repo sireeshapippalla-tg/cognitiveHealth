f37a4b902157e6e11b6f0320db983d291f9d101e
Sireesha P <sireesha.p@reneesystems.com>
2026-02-11 17:19:03 +0530

    bulid issue

--- src/components/home/offering/OurOfferingSection.tsx
+++ src/components/home/offering/OurOfferingSection.tsx
@@ -3,79 +3,67 @@ import { Grid } from "@mui/material";
 import { SectionTitle } from "../../ui/sectionTitle/SectionTitle";
 import { FeatureCard } from "../../ui/featureCard/FeatureCard";
 import { SectionContainer, Wrapper } from "./ourOfferingSection.styles";
-import Vector from "../../../assets/Vector.svg"; 
+import Vector from "../../../assets/Vector.svg";
 import Frame from "../../../assets/Frame.svg";
 import Workflow from "../../../assets/Workflow.svg";
 import Eligibility from "../../../assets/Eligibility.svg";
 import Analysis from "../../../assets/Analysis.svg";
-import PreBill from "../../../assets/PreBill.svg";
-
-
+import PreBill from "../../../assets/Prebill.svg";
 
 const offerings = [
   {
     title: "Cash Posting and Advanced Recon",
     icon: (
-  <img
-    src={Vector}
-    alt="Vector Icon"
-    style={{ width: 24, height: 24 }}
-  />
-),
+      <img src={Vector} alt="Vector Icon" style={{ width: 24, height: 24 }} />
+    ),
 
     link: "#",
   },
   {
     title: "Lockbox Automation",
-   icon: (
-  <img
-    src={Frame}
-    alt="Frame Icon"
-    style={{ width: 24, height: 24 }}
-  />
-),
+    icon: (
+      <img src={Frame} alt="Frame Icon" style={{ width: 24, height: 24 }} />
+    ),
     link: "#",
   },
   {
     title: "Eligibility Discovery ",
-   icon: (
-  <img
-    src={Eligibility}
-    alt="Eligibility Icon"
-    style={{ width: 24, height: 24 }}
-  />
-),
+    icon: (
+      <img
+        src={Eligibility}
+        alt="Eligibility Icon"
+        style={{ width: 24, height: 24 }}
+      />
+    ),
     link: "#",
   },
   {
     title: "Denials Workflow ",
-   icon: (
-  <img
-    src={Workflow}
-    alt="Workflow Icon"
-    style={{ width: 24, height: 24 }}
-  />
-),
+    icon: (
+      <img
+        src={Workflow}
+        alt="Workflow Icon"
+        style={{ width: 24, height: 24 }}
+      />
+    ),
     link: "#",
   },
   {
     title: "Contract Analytics",
     icon: (
-  <img    src={Analysis}
-    alt="Analysis Icon"
-    style={{ width: 24, height: 24 }}
-  />
-),
+      <img
+        src={Analysis}
+        alt="Analysis Icon"
+        style={{ width: 24, height: 24 }}
+      />
+    ),
     link: "#",
   },
   {
     title: "Enhanced Pre-bill services",
     icon: (
-  <img    src={PreBill}
-    alt="PreBill Icon"
-    style={{ width: 24, height: 24 }}
-  />
-),
+      <img src={PreBill} alt="PreBill Icon" style={{ width: 24, height: 24 }} />
+    ),
     link: "#",
   },
 ];
@@ -84,7 +72,12 @@ const OurOfferingSection = () => {
   return (
     <SectionContainer>
       <Wrapper>
-        <SectionTitle title="Our Offering" align="center" maxWidth={700} textColor="var(--color-primary)"/>
+        <SectionTitle
+          title="Our Offering"
+          align="center"
+          maxWidth={700}
+          textColor="var(--color-primary)"
+        />
 
         <Grid container spacing={4}>
           {offerings.map((offering, index) => (
