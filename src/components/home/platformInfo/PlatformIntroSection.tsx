82bbbfbc9a3d207b8bffca25bbe522820aecc158
Dinesh <dineshmunagala97@gmail.com>
2026-02-13 13:25:45 +0530

    Updated Platformintrosection content

--- src/components/home/platformInfo/PlatformIntroSection.tsx
+++ src/components/home/platformInfo/PlatformIntroSection.tsx
@@ -7,7 +7,7 @@ const PlatformIntroSection = () => {
       <Wrapper>
         <SectionTitle
           pillText="The Future of Healthcare Revenue Cycle Is Here – Unified, Smarter, Better"
-          title="<span>CognitiveHealth’s iCAN™</span> Platform: Where Deep <br/> RCM Expertise Meets Advanced AI"
+          title="<span>CognitiveHealth’s iCAN™ ONE</span> Platform: Where Deep <br/> RCM Expertise Meets Advanced AI"
           subtitle="Decades of revenue cycle leadership combined with cutting-edge intelligent automation, machine learning, generative AI, and enterprise-grade infrastructure, purpose-built to solve your toughest operational challenges."
           maxWidth={860}
         />
